a0e1d74a470b2348e49ccf7831e817d353e2c88a
turnjp <>
2017-10-28 22:51:09 +0900

    時間内正解分

---
+++ AtCoder/AtCoderBeginnerContest076/ABC076A.cpp
@@ -0,0 +1,21 @@
+    //問題URL
+    //
+
+    //解き方メモ
+    //
+    
+    #include<iostream>
+    using namespace std;
+
+    int main(){
+        int r,g;
+        int result;
+
+        //R,G入力
+        cin >> r >> g;
+
+        //出力
+        cout << 2*g-r << endl;
+
+        return 0;
+    }
