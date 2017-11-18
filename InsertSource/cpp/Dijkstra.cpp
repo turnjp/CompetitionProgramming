67e4eb0441118d5547ce5ff3aacda475c724c1d1
turnjp <turnjp1212@gmail.com>
2017-11-19 02:55:22 +0900

    最短経路確定済みのノードが探索対象とならないよう修正

--- InsertSource/cpp/Dijkstra.cpp
+++ InsertSource/cpp/Dijkstra.cpp
@@ -1,8 +1,4 @@
 /*** 挿入用ソース ***/
-#include<iostream>
-#include<queue>
-#include<vector>
-using namespace std;
 
 #define MAX_NODE_NUM 10
 
@@ -12,9 +8,11 @@ using namespace std;
 //1.繋がっていない点のコスト（cost[from][to]）は無限大（と考えていい数値）がセットされている
 //2.コストがマイナスの経路は無い
 int dijkstra(int start,int goal,int cost[MAX_NODE_NUM][MAX_NODE_NUM],int node_num){
-  if(start == goal)
-    return 0;
+
+  //探索済みか（最短経路が確定したか）
+  bool searched[node_num];
   
+  //ゴールまでのコスト
   int goal_cost;
 
   //優先度付きキューを使用する
@@ -22,21 +20,37 @@ int dijkstra(int start,int goal,int cost[MAX_NODE_NUM][MAX_NODE_NUM],int node_nu
   //コストが小さい順のキューとなるよう宣言
   priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> q;
 
+  if(start == goal)
+    return 0;
+
+  for (int i = 0; i < node_num; i++) {
+    searched[i] = false;
+  }
+
+  
   //開始点をキューに入れる。コスト０
   q.push(make_pair(0,start));
   while(true){
     //コスト最小（キュー先頭）を取り出す
     pair<int,int> p = q.top();
     q.pop();
+
+    //取り出したノードが探索済み（最短経路確定済み）なら無視する
+    if(searched[p.second]){
+      continue;
+    }
+
+    //現在ノードを探索済み（最短経路確定済み）とする
+    searched[p.second] = true;
     
     if(p.second == goal){
       //ゴールに到達した場合、ループを終了する
       goal_cost = p.first;
       break;
     } else {
-      //ゴールに到達しなかった場合、隣接する点をキューへ追加する
+      //ゴールに到達しなかった場合、最短経路が確定していない隣接するノードをキューへ追加する
       for (int k = 0; k < node_num; k++) {
-	if(k != p.second){
+	if(!searched[k]){
 	  q.push(make_pair(p.first+cost[p.second][k],k));
 	}
       }
