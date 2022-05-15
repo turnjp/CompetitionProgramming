752920bb5516eb349835b46e59bb8eb7d0ca9044
turnjp <turnjp1212@gmail.com>
2022-05-16 00:42:05 +0900

    ARC140時間外回答ソース改良版

--- AtCoder/arc140/arc140_a.cpp
+++ AtCoder/arc140/arc140_a.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int main()
@@ -10,8 +11,10 @@ int main()
     cin >> n >> k;
     cin >> s;
 
+    int result = n;
     int tmp_k;
-    int a_cnt[26]; //アルファベットごとのカウンタ
+    int substr_num;
+    vector<int> a_cnt(26); //アルファベットごとのカウンタ
 
     for (int offset = 1; offset <= n / 2; offset++)
     {
@@ -22,28 +25,28 @@ int main()
 
         //各部分文字列のj番目を文字種類ごとにカウント
         tmp_k = k;
+        substr_num = n/offset;
         for (int j = 0; j < offset && tmp_k >= 0; j++)
         {
-            //アルファベットごとのカウンタ初期化
-            for (int i = 0; i < 26; i++)
-            {
-                a_cnt[i] = 0;
-            }
+            //アルファベットカウンタ初期化
+            fill(a_cnt.begin(),a_cnt.end(),0);
+
             // j2番目の部分文字列のj文字目をカウント
-            for (int j2 = 0; j2 < n / offset; j2++)
+            for (int j2 = 0; j2 < substr_num; j2++)
             {
                 a_cnt[s[j2 * offset + j] - 'a']++;
             }
-            sort(a_cnt, a_cnt + 26);
-            tmp_k -= (n / offset - a_cnt[25]);
+            sort(a_cnt.begin(),a_cnt.end(),greater<int>());
+            tmp_k -= (substr_num - a_cnt[0]);
         }
         if (tmp_k >= 0)
         {
-            cout << offset << endl;
-            return 0;
+            result = offset;
+            break;
         }
     }
-    cout << n << endl;
+
+    cout << result << endl;
 
     return 0;
 }
