d9271f4d0c16828ce1f424e0f53c75426ef91c1d
turnjp <turnjp1212@gmail.com>
2022-05-22 13:32:21 +0900

    ABC252時間外回答

--- AtCoder/abc252/abc252_d.cpp
+++ AtCoder/abc252/abc252_d.cpp
@@ -1,48 +1,19 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
 
-/* ---------------------- ここから ---------------------- */
-// #include <boost/multiprecision/cpp_dec_float.hpp>
-#include <boost/multiprecision/cpp_int.hpp>
-namespace mp = boost::multiprecision;
-// 任意長整数型
-using Bint = mp::cpp_int;
-// 仮数部が10進数で1024桁の浮動小数点数型(TLEしたら小さくする)
-// using Real = mp::number<mp::cpp_dec_float<1024>>;
-/* ---------------------- ここまで ---------------------- */
-
-Bint f(int n)
-{
-    static map<int, Bint> done_map;
-
-    Bint result = 1;
-
-    for (int i = n; i > 1; i--)
-    {
-        if (done_map.count(i) == 1)
-        {
-            result *= done_map[i];
-            break;
-        }
-        result *= i;
-    }
-    done_map[n] = result;
-
-    return result;
-}
+#define LLint long long
 
 int main()
 {
-    int n;
+    LLint n;
 
     cin >> n;
 
-    map<int, int> a_map;
+    map<LLint, LLint> a_map;
 
-    for (int i = 0; i < n; i++)
+    for (LLint i = 0; i < n; i++)
     {
-        int tmp;
+        LLint tmp;
         cin >> tmp;
         if (a_map.count(tmp) == 0)
         {
@@ -54,29 +25,21 @@ int main()
         }
     }
 
-    Bint result = 0;
+    LLint result = (n * (n - 1) * (n - 2)) / 6;
 
-    for (pair<int, int> a : a_map)
+    for (pair<LLint, LLint> a : a_map)
     {
-        int tmp = a.second;
-        Bint tmp3;
+        LLint tmp = a.second;
 
-        if (tmp >= 2)
-        {
-            tmp3 = f(tmp);
-        }
         if (tmp >= 3)
         {
-            result -= tmp3 / (6 * f(tmp - 3));
+            result -= (tmp * (tmp - 1) * (tmp - 2)) / 6;
         }
         if (tmp >= 2 && n - tmp >= 1)
         {
-            Bint tmp2 = f((n - tmp) - 1);
-            result -= (tmp3 / (2 * f(tmp - 2))) * (f(n - tmp) / tmp2);
+            result -= (tmp * (tmp - 1) / 2) * (n - tmp);
         }
     }
-
-    result += f(n) / (6 * f(n - 3));
 
     cout << result << endl;
 
