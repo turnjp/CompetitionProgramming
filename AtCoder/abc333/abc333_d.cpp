e30679d1697628b7d703cc674a3ffbb1c5d198bc
turnjp <turnjp1212@gmail.com>
2023-12-31 18:26:06 +0900

    日を改めて解説読まずに正解

--- AtCoder/abc333/abc333_d.cpp
+++ AtCoder/abc333/abc333_d.cpp
@@ -8,56 +8,62 @@ bool in_mass(int x, int y, int w, int h) {
 const int dx[8] = {0, 0, -1, 1, 1, 1, -1, -1};
 const int dy[8] = {-1, 1, 0, 0, -1, 1, 1, -1};
 
-int rec(int cnt, vector<int> lv, int t, vector<vector<int>> graph) {
-
-    int l = lv[t];
-    if(l == 1) {
-        return cnt;
-    }
-    lv.erase(lv.begin() + t);
-    for(const int p : graph[l]) {
-        graph[p].erase(find(graph[p].begin(), graph[p].end(), l));
-    }
-    graph.erase(graph.begin() + l);
-
-    int min_cnt = INT_MAX;
-
-    for(int i = 0; i < lv.size(); i++) {
-        min_cnt = min(min_cnt, rec(cnt + 1, lv, i, graph));
-    }
-
-    return min_cnt;
-}
-
 int main() {
     int n;
 
     cin >> n;
 
-    vector<vector<int>> graph(n + 1);
+    vector<set<int>> tree(n);
 
     for(int i = 0; i < n - 1; i++) {
         int u, v;
         cin >> u >> v;
-
-        graph[u].push_back(v);
-        graph[v].push_back(u);
+        u -= 1;
+        v -= 1;
+        tree[u].insert(v);
+        tree[v].insert(u);
     }
 
-    vector<int> lv;
+    int result;
+    if(tree[0].size() == 1) { // 1自体が葉の場合
+        result = 1;
+    } else { // 1が枝の場合
+        ////頂点1より下の部分木のノード数の配列
+        vector<int> size_v;
 
-    for(int i = 0; i < graph.size(); i++) {
-        if(graph[i].size() == 1) {
-            lv.push_back(i);
+        // 頂点1より下の部分木のノード数を求める
+        for(auto one_next : tree[0]) {
+            queue<pair<int, int>> q;
+
+            int cnt = 0;
+            q.push({one_next, 0});
+
+            do {
+                int curr = q.front().first;
+                int parent = q.front().second;
+                q.pop();
+                cnt++;
+
+                // 葉でない（＝枝）
+                if(tree[curr].size() > 1) {
+                    for(auto next : tree[curr]) {
+                        if(next != parent) {
+                            q.push({next, curr});
+                        }
+                    }
+                }
+
+            } while(q.size() > 0);
+            size_v.push_back(cnt);
         }
+        // 昇順
+        sort(size_v.begin(), size_v.end());
+
+        // 最大値以外を合算し、頂点１の分を足す
+        result = reduce(size_v.begin(), size_v.end() - 1) + 1;
     }
 
-    int min_cnt = INT_MAX;
-    for(int i = 0; i < lv.size(); i++) {
-        min_cnt = min(min_cnt, rec(1, lv, i, graph));
-    }
-
-    cout << min_cnt << endl;
+    cout << result << endl;
 
     return 0;
 }
