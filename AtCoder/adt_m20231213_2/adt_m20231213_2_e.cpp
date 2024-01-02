3f2f14b0536660f200882e5f32841c22b9df89eb
turnjp <turnjp1212@gmail.com>
2024-01-02 14:14:43 +0900

    解説読んだ上で回答

--- AtCoder/adt_m20231213_2/adt_m20231213_2_e.cpp
+++ AtCoder/adt_m20231213_2/adt_m20231213_2_e.cpp
@@ -11,35 +11,46 @@ using namespace std;
 int main() {
     long long n, k;
     cin >> n >> k;
-    vector<long long> abv[2];
+    vector<long long> av(n);
+    vector<long long> bv(n);
 
-    for(int i = 0; i < 2; i++) {
-        for(int j = 0; j < n; j++) {
-            long long tmp;
-            cin >> tmp;
-            abv[i].push_back(tmp);
-        }
+    for(int i = 0; i < n; i++) {
+        cin >> av[i];
     }
 
-    int cabi[4] = {0, 0, 1, 1};
-    int nabi[4] = {0, 1, 0, 1};
+    for(int i = 0; i < n; i++) {
+        cin >> bv[i];
+    }
 
-    for(int i = 0; i < n - 1; i++) {
-        int j = 0;
-        for(; j < 4; j++) {
-            long long x1 = abv[cabi[j]][i];
-            long long x2 = abv[nabi[j]][i + 1];
-            if(abs(x1 - x2) <= k) {
-                break;
+    vector<bool> a_succ_v(n);
+    vector<bool> b_succ_v(n);
+
+    a_succ_v[0] = true;
+    b_succ_v[0] = true;
+
+    for(int i = 1; i < n; i++) {
+        if(a_succ_v[i - 1]) {
+            if(abs(av[i - 1] - av[i]) <= k) {
+                a_succ_v[i] = true;
+            }
+            if(abs(av[i - 1] - bv[i]) <= k) {
+                b_succ_v[i] = true;
             }
         }
-        if(j == 4) {
-            cout << "No" << endl;
-            return 0;
+        if(b_succ_v[i - 1]) {
+            if(abs(bv[i - 1] - av[i]) <= k) {
+                a_succ_v[i] = true;
+            }
+            if(abs(bv[i - 1] - bv[i]) <= k) {
+                b_succ_v[i] = true;
+            }
         }
     }
-
-    cout << "Yes" << endl;
+    if(a_succ_v[n - 1] || b_succ_v[n - 1]) {
+        cout << "Yes" << endl;
+    } else {
+        cout << "No" << endl;
+    }
 
     return 0;
 }
