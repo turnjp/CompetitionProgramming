c2821f754ef189376633d9bae029a7d499527a06
turnjp <turnjp1212@gmail.com>
2024-01-20 19:00:54 +0900

    解説を読んで簡潔に書き直し

--- AtCoder/adt_m20231227_2/adt_m20231227_2_d.cpp
+++ AtCoder/adt_m20231227_2/adt_m20231227_2_d.cpp
@@ -7,23 +7,18 @@ using namespace std;
 int main() {
     char p, q;
     cin >> p >> q;
+    p -= 'A';
+    q -= 'A';
 
-    map<char, int> n_map;
-
-    n_map['B'] = 3;
-    n_map['C'] = 1;
-    n_map['D'] = 4;
-    n_map['E'] = 1;
-    n_map['F'] = 5;
-    n_map['G'] = 9;
+    vector<int> n = {3, 1, 4, 1, 5, 9};
 
     if(p > q) {
         swap(p, q);
     }
 
     int ans = 0;
-    for(char key = p + 1; key <= q; key++) {
-        ans += n_map[key];
+    for(int i = p; i < q; i++) {
+        ans += n[i];
     }
 
     cout << ans << endl;
